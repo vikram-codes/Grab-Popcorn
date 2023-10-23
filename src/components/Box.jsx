2b967f688faaf094092bddf3a80cfd9f774f8a5a
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-23 14:27:51 +0530

    idk what happened, it was showing for commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <>
+      <div className="box">
+        <button
+          className="btn-toggle"
+          onClick={() => setIsOpen((open) => !open)}
+        >
+          {isOpen ? "–" : "+"}
+        </button>
+        {isOpen && children}
+      </div>
+    </>
+  );
+}
+
+export function MovieList({ movies }) {
+  return (
+    <>
+      <ul className="list">
+        {movies?.map((movie) => (
+          <Movie key={movie.imdbID} movie={movie} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <>
+      <li>
+        <img src={movie.Poster} alt={`${movie.Title} poster`} />
+        <h3>{movie.Title}</h3>
+        <div>
+          <p>
+            <span>🗓</span>
+            <span>{movie.Year}</span>
+          </p>
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default Box;
