b75ad9c130b50b8794cc8860b0a14fc3493fdb3c
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-25 13:30:43 +0530

    Testing StarRating

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./components/StarRating";
 // import App from './App.jsx'
 // import './index.css'
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <>
-      <StarRating maxRating={5} color="blue" onSetRating={setMovieRating} />
-      <p>You have rated {movieRating} star </p>
-    </>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -23,7 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
-    <Test />
     <StarRating maxRating={10} color="red" size={48} className="test" />
   </React.StrictMode>
 );
