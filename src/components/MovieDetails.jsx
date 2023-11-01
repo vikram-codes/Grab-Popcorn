34780be2fc57eabec0a313aa17bd79c979a40cbb
Vikram Saini <vikramjit.personal@gmail.com>
2023-11-01 15:43:22 +0530

    Selecting the ID for displaying

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,7 +1,18 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=6d352d05&i=${selectedId}`
+      );
+      const data = await res.json();
+      setMovie(data);
+    }
+  }, []);
+
   return (
     <>
       <button className="btn-back" onClick={onCloseMovie}>
