314148a0c2a56403b5402c403adec5aef4e5be09
Vikram Saini <vikramjit.personal@gmail.com>
2023-11-01 17:14:00 +0530

    Added Loader before Loading MovieDetails

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,55 +1,63 @@
 /* eslint-disable react/prop-types */
+import Loader from "./Loader";
 import StarRating from "./StarRating";
 import React, { useEffect, useState } from "react";
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [movie, setMovie] = useState({});
   useEffect(() => {
     async function getMovieDetails() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=6d352d05&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
+      setIsLoading(false);
     }
     getMovieDetails();
   }, [selectedId]);
 
   return (
     <>
-      <div className="details">
-        <header>
-          <button className="btn-back" onClick={onCloseMovie}>
-            &larr;
-          </button>
-          <img src={movie.Poster} alt={`Poster of ${movie}`} />
-          <div className="details-overview">
-            <h2>{movie.Title}</h2>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="details">
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={movie.Poster} alt={`Poster of ${movie}`} />
+            <div className="details-overview">
+              <h2>{movie.Title}</h2>
+              <p>
+                {movie.Released} &bull; {movie.Runtime}
+              </p>
+              <p>{movie.Genre}</p>
+              <p>
+                <span>🌟</span>
+                {movie.imdbRating} Imdb Rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <StarRating
+              size="32"
+              maxRating="10"
+              color="orange"
+              className="rating"
+              key={movie.imdbRating}
+            />
             <p>
-              {movie.Released} &bull; {movie.Runtime}
+              <em>{movie.Plot}</em>
             </p>
-            <p>{movie.Genre}</p>
-            <p>
-              <span>🌟</span>
-              {movie.imdbRating} Imdb Rating
-            </p>
-          </div>
-        </header>
-        <section>
-          <StarRating
-            size="32"
-            maxRating="10"
-            color="orange"
-            className="rating"
-            key={movie.imdbRating}
-          />
-          <p>
-            <em>{movie.Plot}</em>
-          </p>
-          <p>Starring {movie.Actors}</p>
-          <p>Directed by {movie.Director}</p>
-        </section>
-      </div>
+            <p>Starring {movie.Actors}</p>
+            <p>Directed by {movie.Director}</p>
+          </section>
+        </div>
+      )}
     </>
   );
 }
